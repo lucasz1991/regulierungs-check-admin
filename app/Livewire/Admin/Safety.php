ef250c11c04ea28a7a0e9870cbfeb00be53f3056
Lucas Zacharias <lucas@zacharias-net.de>
2025-05-14 14:07:41 +0200

    Remove filter to exclude specific user from activity log query

--- app/Livewire/Admin/Safety.php
+++ app/Livewire/Admin/Safety.php
@@ -27,7 +27,6 @@ class Safety extends Component
         $activities = Activity::query()
             ->leftJoin('users', 'users.id', '=', 'activity_log.causer_id')
             ->select('activity_log.*', 'users.name')
-            ->where('activity_log.causer_id', '!=', 1)
             ->when($this->filterMode === 'user', function ($query) {
                 $query->whereNotNull('activity_log.causer_id');
             })

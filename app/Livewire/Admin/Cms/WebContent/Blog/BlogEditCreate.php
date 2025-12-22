105dfaaaa1a4dd320a3452f3c7a75b220eb3c6b5
Lucas Zacharias <lucas@zacharias-net.de>
2025-12-22 17:05:07 +0100

    dfdf

--- app/Livewire/Admin/Cms/WebContent/Blog/BlogEditCreate.php
+++ app/Livewire/Admin/Cms/WebContent/Blog/BlogEditCreate.php
@@ -103,7 +103,7 @@ class BlogEditCreate extends Component
     protected function uploadImageViaMediaController($file)
     {
         // Temporäres Request-Objekt mit dem File als 'file'
-        $request = Request::create('/admin/media/upload', 'POST', ['disk' => $disk], [], ['file' => $file]);
+        $request = Request::create('/admin/media/upload', 'POST', ['disk' => 'public'], [], ['file' => $file]);
 
         // MediaController manuell instanziieren und aufrufen
         $controller = new MediaController();

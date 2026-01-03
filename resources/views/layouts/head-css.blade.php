58daa1971ebb0a5c698e090a7172a617227cf6a3
Lucas Zacharias <lucas@zacharias-net.de>
2026-01-03 15:55:39 +0100

    sdsd

--- resources/views/layouts/head-css.blade.php
+++ resources/views/layouts/head-css.blade.php
@@ -3,10 +3,10 @@
 <link rel="stylesheet" href="{{ URL::asset('build/css/tailwind.min.css') }}" />
 <link rel="stylesheet" href="{{ URL::asset('build/libs/apexcharts/apexcharts.css') }}" />
 <link href="{{ URL::asset('adminresources/flatpickr/flatpickr.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ URL::asset('adminresources/choices.js/public/assets/styles/choices.min.css') }}" rel="stylesheet" type="text/css" />
-        <script src="{{ URL::asset('adminresources/choices.js/public/assets/scripts/choices.min.js') }}"></script>
-        <script src="{{ URL::asset('adminresources/flatpickr/flatpickr.min.js') }}"></script>
-        <script src="{{ URL::asset('adminresources/flatpickr/l10n/de.js') }}"></script>
-               <link rel="stylesheet" href="https://uicdn.toast.com/editor/latest/toastui-editor.min.css">
-        <script src="https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js" defer></script>
-    <link rel="stylesheet" href="{{ asset('adminresources/fontawesome6/css/all.min.css') }}">
+<link href="{{ URL::asset('adminresources/choices.js/public/assets/styles/choices.min.css') }}" rel="stylesheet" type="text/css" />
+<script src="{{ URL::asset('adminresources/choices.js/public/assets/scripts/choices.min.js') }}"></script>
+<script src="{{ URL::asset('adminresources/flatpickr/flatpickr.min.js') }}"></script>
+<script src="{{ URL::asset('adminresources/flatpickr/l10n/de.js') }}"></script>
+<link rel="stylesheet" href="https://uicdn.toast.com/editor/latest/toastui-editor.min.css">
+<script src="https://uicdn.toast.com/editor/latest/toastui-editor-all.min.js" defer></script>
+<link rel="stylesheet" href="{{ asset('adminresources/fontawesome6/css/all.min.css') }}">
